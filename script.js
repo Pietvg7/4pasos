694772d15168b9374014f469fa31ec6a490178f9
Pietvg7 <tandem.pietrovg@gmail.com>
2023-11-28 13:29:39 +0100

    4pasos

---
+++ script.js
@@ -0,0 +1,9 @@
+function nextStep(currentStep, nextStep) {
+    document.getElementById(currentStep).classList.remove('active');
+    document.getElementById(nextStep).classList.add('active');
+}
+
+function prevStep(currentStep, prevStep) {
+    document.getElementById(currentStep).classList.remove('active');
+    document.getElementById(prevStep).classList.add('active');
+}
